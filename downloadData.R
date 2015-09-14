09d045bcdca9221decb86dba548b2ad83d42ca24
reindeers <velokarri@mail.ru>
2015-09-14 12:55:26 +0300

    Create downloadData.R

---
+++ downloadData.R
@@ -0,0 +1,34 @@
+# get data from xml (download)
+
+if (!file.exists("data")) {
+  dir.create("data")
+}
+
+fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xml?accessType=DOWNLOAD"
+download.file(fileUrl, destfile="./data/cameras.xml")
+list.files("./data")
+
+doc <- xmlTreeParse("./data/cameras.xml", useInternal = TRUE)
+rootNode <- xmlRoot(doc)
+xmlName(rootNode)
+names(rootNode)
+rootNode[[1]][[1]][["street"]]
+
+# get data from web
+## with xml parser
+url <- "http://krstc.ru/"
+doc <- htmlTreeParse(url, useInternalNodes = TRUE)
+xpathSApply(doc, "//p", xmlValue)
+
+## with httr pack
+
+library("httr"); doc2 <- GET(url)
+content <- content(doc2, as="text")
+parsedHtml <-htmlParse(content, asText = TRUE)
+
+# with passwords
+
+# using handles
+
+# mining with API's
+## twitter API
